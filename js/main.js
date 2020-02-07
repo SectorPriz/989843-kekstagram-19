0965aa9dcf4042f9ef5fa9d267d708b7ad2f53eb
Алексей <mil-tank@bk.ru>
2020-02-07 15:05:20 +0400

    Создал фрагмент, отрисовал в нем массив со скопированными из темплейта данными

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
 'use strict';
+
+// Создал массив для визуального отображения (примера)
+
+var pictures = [
+  {
+    url: 'photos/1.jpg',
+    description: 'ajnj',
+    likes: 10,
+    comments: 10
+  },
+  {
+    url: 'photos/1.jpg',
+    description: 'ajnj',
+    likes: 10,
+    comments: 10
+  }
+];
+
+// Секция куда будем добавлять картинки
+
+var similarPictureElement = document.querySelector('.pictures');
+
+// Секция откуда берем шаблон (копируем содержимое template)
+
+var similarPictureTemplate = document.querySelector('#picture')
+    .content
+    .querySelector('.picture');
+
+// Копируем данные в объект
+
+var renderPicture = function (picture) {
+  var pictureElement = similarPictureTemplate.cloneNode(true);
+
+  pictureElement.querySelector('.picture__img').src = picture.url;
+  pictureElement.querySelector('.picture__img').alt = picture.description;
+  pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+  pictureElement.querySelector('.picture__comments').textContent = picture.comments;
+
+  return pictureElement;
+};
+
+// Создаем фрагмент и отрисовываем в нем массив pictures
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < pictures.length; i++) {
+  fragment.appendChild(renderPicture(pictures[i]));
+}
+
+similarPictureElement.appendChild(fragment);
