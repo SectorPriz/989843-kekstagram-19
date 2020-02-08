55a6f6dfadebe65fa0015b2359b070d0959d58a6
Алексей <mil-tank@bk.ru>
2020-02-08 21:42:08 +0400

    Получил рандомные уникальные индексы фотографий

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 'use strict';
 
+// Создадем массив имен комментаторов
 
-// Cоздадим массив комментариев
+var NAME_COMMENTATORS = ['Артем', 'Алексей', 'Елена', 'Гарри', 'Артемида', 'Кощей'];
+
+// Создаем функцию для получения рандомного индекса массива
+
+var generateRandom = function (arr) {
+  var rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
+};
+
+// Cоздадем массив комментариев
 
 var COMMENTS = [];
 var commentsNumber = 6;
@@ -9,19 +19,34 @@ for (i = 0; i < commentsNumber; i++) {
   COMMENTS.push({
     avatar: 'img/avatar-6.svg',
     message: 'В целом всё неплохо. Но не всё.',
-    name: 'Артем'
+    name: generateRandom(NAME_COMMENTATORS)
   });
 }
 
-// Получаем массив случайных фотографий (ПОКА С ВОЗМОЖНОСТЬЮ ПОВТОРЕНИЯ)
+// Получаем массив случайных фотографий (посмотри вникни!!!!(для себя))
 
-var PHOTO = [];
+var RANDOM_NUMBER_PHOTO = [];
+var ORDERED_NUMBER_PHOTO = [];
 var photoNumber = 25;
-var getRandomPhoto = function () {
-  return Math.floor(Math.random() * (25) + 1);
+var getRandomIndex = function (maxNumber) {
+  return Math.floor(Math.random() * maxNumber);
 };
-for (i = 0; i <= photoNumber; i++) {
-  PHOTO.push('photos/' + getRandomPhoto() + '.jpg');
+
+// Создаем цикл для получения массива с упорядоченными номерами фотографий
+
+for (i = 1; i <= photoNumber; i++) {
+  ORDERED_NUMBER_PHOTO.push(i);
+}
+
+// Создаем цикл для получения рандомного уникального индекса фотографии (как-то так)
+
+for (i = 0; i < photoNumber; i++) {
+  var maxIndexNumberCopy = ORDERED_NUMBER_PHOTO.length - 1;
+  var rand = getRandomIndex(maxIndexNumberCopy);
+  var numberPhoto = ORDERED_NUMBER_PHOTO[rand];
+  ORDERED_NUMBER_PHOTO[rand] = ORDERED_NUMBER_PHOTO[maxIndexNumberCopy];
+  ORDERED_NUMBER_PHOTO.pop();
+  RANDOM_NUMBER_PHOTO.push(numberPhoto);
 }
 
 // Получаем случайное число лайков в диапазоне от 15 до 250
@@ -30,13 +55,13 @@ var getRandomLikes = function () {
   return Math.floor(Math.random() * (250 - 15) + 1) + 15;
 };
 
-// Создал массив для визуального отображения (примера)
+// Создаем массив фотографий в рандомном порядке
 
 var PICTURES = [];
 var picturesNumber = 25;
 for (var i = 0; i < picturesNumber; i++) {
   PICTURES.push({
-    url: PHOTO[i],
+    url: 'photos/' + RANDOM_NUMBER_PHOTO[i] + '.jpg',
     description: 'описание фотографии',
     likes: getRandomLikes(),
     comments: COMMENTS.length
@@ -66,7 +91,7 @@ var renderPicture = function (picture) {
   return pictureElement;
 };
 
-// Создаем фрагмент и отрисовываем в нем массив pictures
+// Создаем фрагмент и отрисовываем массив в .pictures
 
 var fragment = document.createDocumentFragment();
 for (i = 0; i < PICTURES.length; i++) {
