f8985fbad519cd054623812be537ddaa269757d8
Алексей <mil-tank@bk.ru>
2020-02-09 16:52:51 +0400

    ДЗ на проверку

--- js/main.js
+++ js/main.js
@@ -1,78 +1,66 @@
 'use strict';
 
-// Создадем массив имен комментаторов
+var comments = [];
+var COMMENTS_NUMBER = 6;
+var AVATAR_NAMBER = 6;
+var pictures = [];
+var PICTURES_NUMBER = 25;
+var numbersPhotos = [];
+var PHOTO_NUMBER = 25;
+var nameOfCommentators = ['Артем', 'Алексей', 'Елена', 'Гарри', 'Артемида', 'Кощей'];
+var commentsMessage = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
-var NAME_COMMENTATORS = ['Артем', 'Алексей', 'Елена', 'Гарри', 'Артемида', 'Кощей'];
+// Функция для получения рандомного индекса массива
 
-// Создаем функцию для получения рандомного индекса массива
-
-var generateRandom = function (arr) {
+var generateRandomIndex = function (arr) {
   var rand = Math.floor(Math.random() * arr.length);
   return arr[rand];
 };
 
-// Cоздадем массив комментариев
+// Функция для получения рандомного числа
 
-var COMMENTS = [];
-var commentsNumber = 6;
-for (i = 0; i < commentsNumber; i++) {
-  COMMENTS.push({
-    avatar: 'img/avatar-6.svg',
-    message: 'В целом всё неплохо. Но не всё.',
-    name: generateRandom(NAME_COMMENTATORS)
-  });
-}
-
-// Получаем массив случайных фотографий (посмотри вникни!!!!(для себя))
-
-var RANDOM_NUMBER_PHOTO = [];
-var ORDERED_NUMBER_PHOTO = [];
-var photoNumber = 25;
-var getRandomIndex = function (maxNumber) {
-  return Math.floor(Math.random() * maxNumber);
+var getRandomNumber = function (number) {
+  return Math.floor(Math.random() * number + 1);
 };
 
-// Создаем цикл для получения массива с упорядоченными номерами фотографий
-
-for (i = 1; i <= photoNumber; i++) {
-  ORDERED_NUMBER_PHOTO.push(i);
-}
-
-// Создаем цикл для получения рандомного уникального индекса фотографии (как-то так)
-
-for (i = 0; i < photoNumber; i++) {
-  var maxIndexNumberCopy = ORDERED_NUMBER_PHOTO.length - 1;
-  var rand = getRandomIndex(maxIndexNumberCopy);
-  var numberPhoto = ORDERED_NUMBER_PHOTO[rand];
-  ORDERED_NUMBER_PHOTO[rand] = ORDERED_NUMBER_PHOTO[maxIndexNumberCopy];
-  ORDERED_NUMBER_PHOTO.pop();
-  RANDOM_NUMBER_PHOTO.push(numberPhoto);
-}
-
-// Получаем случайное число лайков в диапазоне от 15 до 250
+// Функция для получения рандомного числа лайков в диапазоне от 15 до 250
 
 var getRandomLikes = function () {
   return Math.floor(Math.random() * (250 - 15) + 1) + 15;
 };
 
-// Создаем массив фотографий в рандомном порядке
+// Массив комментариев
 
-var PICTURES = [];
-var picturesNumber = 25;
-for (var i = 0; i < picturesNumber; i++) {
-  PICTURES.push({
-    url: 'photos/' + RANDOM_NUMBER_PHOTO[i] + '.jpg',
-    description: 'описание фотографии',
-    likes: getRandomLikes(),
-    comments: COMMENTS.length
+for (i = 0; i < getRandomNumber(COMMENTS_NUMBER); i++) {
+  comments.push({
+    avatar: 'img/avatar-' + getRandomNumber(AVATAR_NAMBER) + '.svg',
+    message: generateRandomIndex(commentsMessage) + ' ' + generateRandomIndex(commentsMessage),
+    name: generateRandomIndex(nameOfCommentators)
   });
 }
 
-// Находим секцию куда будем добавлять  объекты (картинки)
+// Получаем массив номеров фотографий
+
+for (i = 1; i <= PHOTO_NUMBER; i++) {
+  numbersPhotos.push(i);
+}
+
+// Массив фотографий
+
+for (var i = 0; i < PICTURES_NUMBER; i++) {
+  pictures.push({
+    url: 'photos/' + numbersPhotos[i] + '.jpg',
+    description: 'описание фотографии',
+    likes: getRandomLikes(),
+    comments: getRandomNumber(COMMENTS_NUMBER)
+  });
+}
+
+// Секция куда будем добавлять  объекты (картинки)
 
 var similarPictureElement = document.querySelector('.pictures');
 
-// Находим секцию откуда берем шаблон (копируем содержимое template)
+// Секция откуда берем шаблон (копируем содержимое template)
 
 var similarPictureTemplate = document.querySelector('#picture')
     .content
@@ -94,8 +82,8 @@ var renderPicture = function (picture) {
 // Создаем фрагмент и отрисовываем массив в .pictures
 
 var fragment = document.createDocumentFragment();
-for (i = 0; i < PICTURES.length; i++) {
-  fragment.appendChild(renderPicture(PICTURES[i]));
+for (i = 0; i < pictures.length; i++) {
+  fragment.appendChild(renderPicture(pictures[i]));
 }
 
 similarPictureElement.appendChild(fragment);
